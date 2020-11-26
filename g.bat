a2e382f8c4371773bee410564d0bcd7a35406824
skandau <73341866+skandau@users.noreply.github.com>
2020-11-27 07:32:20 +0800

    Add files via upload

---
+++ g.bat
@@ -0,0 +1,28 @@
+@echo off
+
+
+
+set incs= -DWIN32 -Izlib -DWINDOWS 
+
+set opts= -fomit-frame-pointer 
+rem -fno-stack-protector -fno-stack-check -fno-check-new
+
+rem -flto -ffat-lto-objects -Wl,-flto -fuse-linker-plugin -Wl,-O -Wl,--sort-common -Wl,--as-needed -ffunction-sections
+rem -fprofile-use -fprofile-correction  -fwhole-program 
+
+rem set gcc=C:\MinGW\bin\g++.exe
+rem set gcc=C:\MinGW\bin\g++.exe
+set gcc=C:\mingw\bin\g++.exe
+set path=%gcc%\..\
+
+
+
+%gcc%\..\gcc.exe -c -march=k8 -O9 %incs% %opts% @list_c
+
+%gcc% -s -march=k8 -O9 %incs% %opts% @list_cpp @list_o -static -o paq8pxd64.exe
+rem -std=gnu++1z
+
+del *.o
+pause
+
+
